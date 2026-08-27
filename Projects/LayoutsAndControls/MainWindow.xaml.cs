b1eebece8643ff1aa454cd1ba54e3d70a9446888
Francisco Lima <francisco.lima@mail.nidec.com>
2026-08-27 09:10:10 +0100

    Not functional example of data bindings and INotifyPropertyChanged interface based on MVVM design pattern.

---
+++ Projects/LayoutsAndControls/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LayoutsAndControls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MyWindow : Window
+    {
+        public MyWindow()
+        {
+            InitializeComponent();
+        }
+
+        void button_click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello");
+            MessageBox.Show("Bahh");
+        }
+    }
+}
